e3ce5bbe7ee28b37456e1a28031cbf5569990f95
silvi12 <silviabaranan12@gmail.com>
2025-10-18 12:14:46 +0200

    cambios en actividades

---
+++ js/js.js
@@ -0,0 +1,17 @@
+function openModal(imageSrc) {
+    const modal = document.getElementById('imageModal');
+    const modalImg = document.getElementById('modalImage');
+    modal.style.display = 'block';
+    modalImg.src = imageSrc;
+}
+
+function closeModal() {
+    document.getElementById('imageModal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById('imageModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
